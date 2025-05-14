ee58e18d885ca1928342d500a67728af69077912
João Gabriel <130689562+GabrielJoao19@users.noreply.github.com>
2025-05-14 17:13:19 +0000

    quinto commit

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
-    public static void main(String[]args){
-
+    public static void main(String[]args)
+    {
         Aluno aluno1 = new Aluno("Joao",70);
+        Retangulo retangulo1 = new Retangulo(5, 5);
 
         aluno1.aprovado();
+        double area_retangulo1 = retangulo1.getArea();
+        System.out.println(area_retangulo1);
+        
     }
 }
